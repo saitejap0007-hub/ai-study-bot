2c8953ef2e4bca150730969ab8b49ce077cb0370
saitejap0007-hub <saitejap0007@gmail.com>
2026-09-17 23:12:30 +0530

    commit changes

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+# Ensure Python finds the backend folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
@@ -12,4 +13,3 @@ app = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    
